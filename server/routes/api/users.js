87d5463ea9437cf9578efee56e213ee2075e4331
AndresE50 <elpanatitolicores@gmail.com>
2024-11-02 06:40:49 +0930

    fix the dependency issue

--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -5,7 +5,6 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const config = require('config');
 const { check, validationResult } = require('express-validator');
-const normalize = require('normalize-url');
 
 const User = require('../../models/User');
 
@@ -37,19 +36,9 @@ router.post(
           .json({ errors: [{ msg: 'User already exists' }] });
       }
 
-      const avatar = normalize(
-        gravatar.url(email, {
-          s: '200',
-          r: 'pg',
-          d: 'mm'
-        }),
-        { forceHttps: true }
-      );
-
       user = new User({
         name,
         email,
-        avatar,
         password
       });
 
